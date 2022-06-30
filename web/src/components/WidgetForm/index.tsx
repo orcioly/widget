0b38fcbabd217d077c5909b883fe1b63871103e0
Orcioly Andrade Alves <orcioly@gmail.com>
2022-06-30 11:55:38 -0300

    Name Dev

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
 
 
       <footer className="text-xs text-neutral-400">
-        Feito com ❤️ por <a className="underline underline-offset-2" href="https://linkedin.com/in/orcioly">Orcioly</a>
+        Desenvolvido por <a className="underline underline-offset-2" href="https://linkedin.com/in/orcioly">Orcioly Andrade Alves</a>
       </footer>
     </div >
   )
